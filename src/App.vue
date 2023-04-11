3e93b7f520835d305022b867a6ab5114cc1b763c
martinapiglio <martina.piglio@gmail.com>
2023-04-11 10:49:49 +0200

    scaffolding: components + style variables/mixins created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script>
+import { store } from './store';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    }
+}
 </script>
 
 <template>
